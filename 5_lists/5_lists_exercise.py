2779ae5e750d2c437e829022d71a9d5f69140d16
Darshan Mahajan <darshan@dotminds.in>
2024-03-28 09:27:17 +0530

    exercise 5 done

--- 5_lists/5_lists_exercise.py
+++ 5_lists/5_lists_exercise.py
@@ -18,23 +18,26 @@
 exp = [2200,2350,2600,2130,2190]
 
 # 1. In Feb, how many dollars you spent extra compare to January?
-print("In feb this much extra was spent compared to jan:",exp[1]-exp[0]) # 150
+# 150
+print("Feb expenses: ",abs(exp[0] - exp[1]))
 
 # 2. Find out your total expense in first quarter (first three months) of the year
-print("Expense for first quarter:",exp[0]+exp[1]+exp[2]) # 7150
-
+# 7150
+print("Expenses in first quarter: ", sum(exp[:3]))
 # 3. Find out if you spent exactly 2000 dollars in any month
-print("Did I spent 2000$ in any month? ", 2000 in exp) # False
-
+# False
+print("Exactly 2000 expense in any month: ", 2000 in exp)
 # 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+# [2200, 2350, 2600, 2130, 2190, 1980]
 exp.append(1980)
-print("Expenses at the end of June:",exp) # [2200, 2350, 2600, 2130, 2190, 1980]
+print(exp)
 
 # 5. You returned an item that you bought in a month of April and
 # got a refund of 200$. Make a correction to your monthly expense list
 # based on this
+# [2200, 2350, 2600, 1930, 2190, 1980]
 exp[3] = exp[3] - 200
-print("Expenses after 200$ return in April:",exp) # [2200, 2350, 2600, 1930, 2190, 1980]
+print(exp)
 
 # 2. You have a list of your favourite marvel super heros
 # heros=['spider man','thor','hulk','iron man','captain america']
@@ -42,19 +45,19 @@ print("Expenses after 200$ return in April:",exp) # [2200, 2350, 2600, 1930, 219
 
 heros=['spider man','thor','hulk','iron man','captain america']
 # 1. Length of the list
-print(len(heros))
+print("Length: ", len(heros))
 # 2. Add 'black panther' at the end of this list
 heros.append('black panther')
 print(heros)
-# 3. You realize that you need to add 'black panther' after 'hulk',
+# 3. You realize that you need to add 'black panther' after 'hulk', 
 # so remove it from the list first and then add it after 'hulk'
-heros.remove('black panther')
+heros.pop()
 heros.insert(3,'black panther')
 print(heros)
 # 4. Now you don't like thor and hulk because they get angry easily :)
 #    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
 #    Do that with one line of code.
-heros[1:3]=['doctor strange']
+heros[1:3] = ['doctor strange']
 print(heros)
 # 5. Sort the list in alphabetical order
 heros.sort()
