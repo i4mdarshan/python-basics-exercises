7c0e9f1e8debb2884c7afc05aa36e4890ca649f9
Darshan Mahajan <darshan@dotminds.in>
2024-03-29 10:42:09 +0530

    exercise 13 done

--- 13_read_write_files/exercise_1_poem.py
+++ 13_read_write_files/exercise_1_poem.py
@@ -1,21 +1,19 @@
-word_stats={}
+poem = open("E:\\Projects\\python-basics-exercises\\13_read_write_files\\poem.txt","r")
 
-with open("poem.txt","r") as f:
-    for line in f:
-      words=line.split(' ')
-      for word in words:
-        if word in word_stats:
-          word_stats[word]+=1
+word_bag = {}
+
+for line in poem:
+    words = line.split()
+    for word in words:
+        if word in word_bag:
+            word_bag[word]+=1
         else:
-          word_stats[word] = 1
+            word_bag[word] = 1
 
-print(word_stats)
+poem.close()
+words_count = list(word_bag.values())
+max_occurence = max(words_count)
 
-word_occurances = list(word_stats.values())
-max_count = max(word_occurances)
-print("Max occurances of any word is:",max_count)
-
-print("Words with max occurances are: ")
-for word, count in word_stats.items():
-    if count==max_count:
-        print(word)
+for word,count in word_bag.items():
+    if count == max_occurence:
+        print(f"{word} => {count}") 
--- 13_read_write_files/exercise_2_stocks.py
+++ 13_read_write_files/exercise_2_stocks.py
@@ -1,12 +1,15 @@
-with open("stocks.csv", "r") as f, open("output.csv", "w") as out:
-    out.write("Company Name,PE Ratio, PB Ratio\n")
-    next(f)  # This will skip first line in the file which is a header
-    for line in f:
-        tokens = line.split(",")
-        stock = tokens[0]
-        price = float(tokens[1])
-        eps = float(tokens[2])
-        book = float(tokens[3])
-        pe = round(price / eps, 2)
-        pb = round(price / book, 2)
-        out.write(f"{stock},{pe},{pb}\n")
+stocks = open("E:\\Projects\\python-basics-exercises\\13_read_write_files\\stocks.csv", "r")
+output = open("E:\\Projects\\python-basics-exercises\\13_read_write_files\\output.csv", "w")
+output.write("Company_Name, PE_Ratio, PB_Ratio\n")
+next(stocks)
+
+for line in stocks:
+    values = line.split(",")
+    company_name = values[0]
+    price = float(values[1])
+    earnings_per_share = float(values[2])
+    book_value = float(values[3])
+    pe_ratio = round(price / earnings_per_share,2)
+    pb_ratio = round(price / book_value,2)
+    output.write(f"{company_name},{pe_ratio},{pb_ratio}\n")
+
